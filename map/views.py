0fa72c4a8f3adc411381a28846c2f06236afba20
MaxSmile44 <afmaxim89@gmail.com>
2025-05-30 13:59:55 +0300

    Add blank start page

--- map/views.py
+++ map/views.py
@@ -3,12 +3,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('''<!DOCTYPE html>
-<html>
-<head>
-  <title>Стартовая</title>
-</head>
-<body>
-  <h1>Здесь будет карта</h1>
-</body>
-</html>''')
+    return render(request, 'map/index.html')
