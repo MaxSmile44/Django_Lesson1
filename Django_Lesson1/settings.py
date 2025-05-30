83fac33c6554ebc2f7c71725518fe3750f705fa9
MaxSmile44 <afmaxim89@gmail.com>
2025-05-30 15:07:26 +0300

    Renamed app map to places

--- Django_Lesson1/settings.py
+++ Django_Lesson1/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'map.apps.MapConfig',
+    'places.apps.PlacesConfig',
 ]
 
 MIDDLEWARE = [
