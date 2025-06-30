9e08ef2cfabf2b751c8f82df99536e6d05af49c3
MaxSmile44 <afmaxim89@gmail.com>
2025-06-30 16:44:48 +0300

    Setup media files serving

--- places/urls.py
+++ places/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 from .scripts import get_images, get_places
@@ -5,7 +7,7 @@ from .scripts import get_images, get_places
 
 urlpatterns = [
     path('', views.index, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 get_places()
 get_images()
