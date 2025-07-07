f8a4af5dfda3325876f4ff5bfac18b547433d294
MaxSmile44 <afmaxim89@gmail.com>
2025-07-07 16:32:39 +0300

    Add slug in model Place

--- places/models.py
+++ places/models.py
@@ -6,6 +6,7 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     title = models.CharField(verbose_name='Название', max_length=200)
+    slug = models.SlugField(max_length=255, unique=True)
     description_short = models.TextField(verbose_name='Краткое описание')
     description_long = models.TextField(verbose_name='Подробное описание')
     lat = models.FloatField(verbose_name='Координаты: широта')
