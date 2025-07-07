d930a6c67c414c8bb8e03b38a7d85be774e235ac
MaxSmile44 <afmaxim89@gmail.com>
2025-07-07 18:17:41 +0300

    Optimized scripts.py

--- places/scripts.py
+++ places/scripts.py
@@ -8,17 +8,17 @@ from pathlib import Path
 from places.models import *
 
 
-def get_images():
-    places = Place.objects.all()
-    images = Image.objects.all()
-    directory = 'places/static/places/places'
-    file_names = [_ for _ in os.listdir(directory) if _.endswith('.json')]
-    for name in file_names:
-        with open(f'places/static/places/places/{name}', 'r', encoding='utf-8') as file:
-            new_img = json.load(file)
-        for i, link in enumerate(new_img['imgs'], 1):
+def add_places(file_names, places, new_object, name):
+    places.create(title=new_object['title'], slug=os.path.splitext(name)[0],
+                  description_short=new_object['description_short'],
+                  description_long=new_object['description_long'], lat=new_object['coordinates']['lat'],
+                  lng=new_object['coordinates']['lng'])
+
+
+def add_images(file_names, places, images, new_object):
+        for i, link in enumerate(new_object['imgs'], 1):
             url = link
-            directory = Path(f"./media/places/{new_img['title']}")
+            directory = Path(f"./media/places/{new_object['title']}")
             if not directory.exists():
                 directory.mkdir(parents=True)
             response = requests.get(url)
@@ -26,18 +26,18 @@ def get_images():
             file_name = f"{i}{os.path.splitext(url.split('/')[-1])[1]}"
             file_names = [_ for _ in os.listdir(directory)]
             if file_name not in file_names:
-                images.create(place=places.get(title=new_img['title']),
+                images.create(place=places.get(title=new_object['title']),
                               image=ContentFile(response.content, name=file_name))
 
 
-def get_places():
+def add_to_db():
     places = Place.objects.all()
+    images = Image.objects.all()
     directory = 'places/static/places/places'
     file_names = [_ for _ in os.listdir(directory) if _.endswith('.json')]
     for name in file_names:
         with open(f'places/static/places/places/{name}', 'r', encoding='utf-8') as file:
-            new_place = json.load(file)
-        if new_place['title'] not in places.values_list('title', flat=True).distinct():
-            places.create(title=new_place['title'], slug=os.path.splitext(name)[0], description_short=new_place['description_short'],
-                                 description_long=new_place['description_long'], lat=new_place['coordinates']['lat'],
-                                 lng=new_place['coordinates']['lng'])
+            new_object = json.load(file)
+        if new_object['title'] not in places.values_list('title', flat=True).distinct():
+            add_places(file_names, places, new_object, name)
+            add_images(file_names, places, images, new_object)
--- places/urls.py
+++ places/urls.py
@@ -2,12 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
-from .scripts import get_images, get_places
+from .scripts import add_to_db
 
 
 urlpatterns = [
     path('', views.index, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-get_places()
-get_images()
+add_to_db()
