e28c641607f2fa29c495d3c3e4e7e83245db0ff7
MaxSmile44 <afmaxim89@gmail.com>
2025-07-11 12:39:16 +0300

    Delete scripts.py

--- places/urls.py
+++ places/urls.py
@@ -3,12 +3,9 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
-from .scripts import add_places_with_images
 
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('places/<int:place_id>', views.places, name='places'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-add_places_with_images()
