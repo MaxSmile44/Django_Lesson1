bf534ead3e49f0aaf01cb980496ec07f98ac7e06
MaxSmile44 <afmaxim89@gmail.com>
2025-07-14 16:25:34 +0300

    Add append slash to the url pattern

--- places/urls.py
+++ places/urls.py
@@ -7,5 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('places/<int:place_id>', views.places, name='places'),
+    path('places/<int:place_id>/', views.places, name='places'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
