d85d984f53a5bd4549adbd9fa2ed9a1ab7d8fbe7
MaxSmile44 <afmaxim89@gmail.com>
2025-07-14 16:11:47 +0300

    Made 'SECRET_KEY' mandatory for starting the project

--- Django_Lesson1/settings.py
+++ Django_Lesson1/settings.py
@@ -8,7 +8,7 @@ env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = env.bool('DEBUG', True)
 
