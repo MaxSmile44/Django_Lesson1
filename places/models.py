07f20b2843aa1a3248e3fde71cc42db2c68e47b1
MaxSmile44 <afmaxim89@gmail.com>
2025-07-14 17:52:08 +0300

    Made field 'image' mandatory in models.py

--- places/models.py
+++ places/models.py
@@ -21,7 +21,7 @@ def get_directory_path(instance, filename):
 
 class Image(models.Model):
     place = models.ForeignKey('Place', verbose_name='Место', related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(verbose_name='Фото', upload_to=get_directory_path, default='')
+    image = models.ImageField(verbose_name='Фото', upload_to=get_directory_path)
 
     def preview(self):
         return format_html(f'<img src="{self.image.url}" style="max-height: 200px;" />')
