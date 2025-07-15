e4c7b2cf46d65848f1c05476af8738537e21affa
MaxSmile44 <afmaxim89@gmail.com>
2025-07-15 12:00:57 +0300

    Update load_place.py

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -2,7 +2,7 @@ import requests
 
 from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
-from pathlib import Path
+from django.shortcuts import get_object_or_404
 
 from places.models import Place, Image
 
@@ -16,27 +16,26 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         link = options['link']
         try:
-            places = Place.objects.all()
             response = requests.get(link)
             response.raise_for_status()
             new_object = response.json()
-            if new_object['title'] not in places.values_list('title', flat=True).distinct():
-                images = Image.objects.all()
-                places.create(title=new_object['title'],
-                              short_description=new_object['description_short'],
-                              long_description=new_object['description_long'],
-                              lat=new_object['coordinates']['lat'],
-                              lng=new_object['coordinates']['lng'])
+            if new_object['title'] not in Place.objects.values_list('title', flat=True).distinct():
+                Place.objects.get_or_create(
+                    title=new_object['title'],
+                    short_description=new_object['description_short'],
+                    long_description=new_object['description_long'],
+                    lat=new_object['coordinates']['lat'],
+                    lng=new_object['coordinates']['lng']
+                )
 
-                directory = Path(f"./media/places/{new_object['title']}")
-                if not directory.exists():
-                    directory.mkdir(parents=True)
                 for link in new_object['imgs']:
                     response = requests.get(link)
                     response.raise_for_status()
                     image_name = link.split('/')[-1]
-                    images.create(place=places.get(title=new_object['title']),
-                                  image=ContentFile(response.content, name=image_name))
+                    Image.objects.get_or_create(
+                        place=get_object_or_404(Place, title=new_object['title']),
+                        image=ContentFile(response.content, name=image_name)
+                    )
                 self.stdout.write(f'Successfully load file from link: {new_object["title"]}')
             else:
                 self.stdout.write(f'This place has already been added earlier')
