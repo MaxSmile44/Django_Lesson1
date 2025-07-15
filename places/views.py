41e7e1df709f11133876f20b9a90831fa2499958
MaxSmile44 <afmaxim89@gmail.com>
2025-07-15 14:57:54 +0300

    Update detailsUrl from def index in views.py - now it referring to details from def places

--- places/views.py
+++ places/views.py
@@ -1,15 +1,16 @@
+import requests
+
 from django.conf import settings
 from django.db.models import Prefetch
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from places.models import Place, Image
 
 
 def index(request):
-    places = Place.objects.prefetch_related(
-        Prefetch('images', queryset=Image.objects.order_by('pk'))
-    )
+    places = Place.objects.all()
     features = [
         {
             'type': 'Feature',
@@ -20,15 +21,9 @@ def index(request):
             'properties': {
                 'title': place.title,
                 'placeId': place.title,
-                'detailsUrl': {
-                    'title': place.title,
-                    'short_description': place.short_description,
-                    'long_description': place.long_description,
-                    'imgs': [
-                        request.build_absolute_uri(place_image.image.url)
-                        for place_image in place.images.all()
-                    ],
-                },
+                'detailsUrl': requests.get(
+                    f"http://127.0.0.1:8000/{reverse('places', kwargs={'place_id': place.id})}"
+                ).json()
             }
         } for place in places
     ]
@@ -40,10 +35,7 @@ def places(request, place_id):
     place = get_object_or_404(Place.objects.prefetch_related(
         Prefetch('images', queryset=Image.objects.order_by('pk'))
     ), pk=place_id)
-    imgs = [
-        request.build_absolute_uri(place_image.image.url)
-        for place_image in place.images.all()
-    ]
+    imgs = [f'{settings.MEDIA_URL}{place_image.image}' for place_image in place.images.all()]
     details = {
         'title': place.title,
         'imgs': imgs,
@@ -51,7 +43,4 @@ def places(request, place_id):
         'long_description': place.long_description,
         'coordinates': {'lng': place.lng, 'lat': place.lat},
     }
-    return JsonResponse(details, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
-
-
-
+    return JsonResponse(details, safe=True, json_dumps_params={'ensure_ascii': False, 'indent': 2})
